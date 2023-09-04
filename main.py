b28a75d4e84b6db4a440e3cfefb1e5f33631a901
antonovmike <antonovma@outlook.com>
2023-09-04 08:49:35 +0400

    error handlers

--- main.py
+++ main.py
@@ -28,28 +28,38 @@ def start(message):
 
 
 def process_places(user_location):
-    cursor.execute("SELECT * FROM places")
-    places = cursor.fetchall()
-    distances = []
-    for place in places:
-        distances.append((place, geodesic(user_location, (place[3], place[4])).km))
+    try:
+        cursor.execute("SELECT * FROM places")
+        places = cursor.fetchall()
+    except psycopg2.Error as err:
+        print(f"Database error: {err}")
+        return []
+    distances = [(place, geodesic(user_location, (place[3], place[4])).km) for place in places]
     distances.sort(key=lambda x: x[1])
     return distances[:2]
 
 
 def send_place_info(message, place):
-    photo = open(f"{place[6]}", 'rb')
+    try:
+        photo = open(f"{place[6]}", 'rb')
+    except FileNotFoundError:
+        print(f"File {place[6]} not found")
+        return
     bot.send_photo(message.chat.id, photo)
     bot.send_message(message.chat.id, f"{place[0]}\n{place[1]}\n{place[5]}")
     bot.send_message(message.chat.id, f"Google map:\n{place[2]}")
     bot.send_venue(message.chat.id, place[3], place[4], f"{place[0]}", f"{place[1]}")
 
 
-with psycopg2.connect(
-    host="localhost",
-    database="telegram_db",
-    user="tg_bot",
-    password="qwerty"
-) as connection:
-    cursor = connection.cursor()
-    bot.polling()
+try:
+    with psycopg2.connect(
+        host="localhost",
+        database="telegram_db",
+        user="tg_bot",
+        password="qwerty"
+    ) as connection:
+        cursor = connection.cursor()
+        bot.polling()
+except psycopg2.OperationalError as err:
+    print(f"Unable to connect to the database: {err}")
+
